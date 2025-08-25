05f558e9b21b24d69889099edfce1c482a90a3b8
Sui Gn <natsynapses@gmail.com>
2025-08-25 00:30:01 -0500

    update

--- cleaker.js
+++
@@ -1,5 +0,0 @@
-const isBrowser = typeof window !== "undefined";
-const cleaker = isBrowser
-  ? await import("./src/cleaker.browser.js").then(m => m.default)
-  : await import("./src/cleaker.node.js").then(m => m.default);
-export default cleaker;
---
+++ index.js
@@ -0,0 +1,4 @@
+// index.js
+import cleaker from './src/cleaker.js';
+export default cleaker;
+console.log('[cleaker] loaded.');
